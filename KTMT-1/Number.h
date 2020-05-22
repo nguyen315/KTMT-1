1a3a4c7c3bc3add94c6c5416ba2a1598d5636b63
Ho Minh Nguyen <hominhnguyen315@gmail.com>
2020-05-22 20:08:35 +0700

    Update Input and Get of Bin, Dec, Hex

--- KTMT-1/Number.h
+++ KTMT-1/Number.h
@@ -3,16 +3,29 @@
 
 #include <vector>
 #include <string>
+#include <unordered_map>
 
 using namespace std;
 
 class Number
 {
+private:
+	static string twoPowerX[128];
+	static unordered_map<char, string> hexMap;
 public:
 	static vector<bool> inputBinary(string number);
 	static vector<bool> inputDecimal(string number);
 	static vector<bool> inputHexa(string number);
+
 	static string div2(string number);
+	static string mult2(string number);
+
+	static string getTwoPowerX(int x);
+	static string sum(string x, string y);
+
+	static unordered_map<char, string> initHexMap();
+	static char getHexFromBinary(string bin);
+	static void toTwoComplement(vector<bool>& arrBits);
 	static void removeZeroPrefix(vector<bool>&);
 };
 
