4f2c25d019d9bceace395a8ce1261a38ddf4b43b
Ho Minh Nguyen <hominhnguyen315@gmail.com>
2020-05-27 20:08:03 +0700

    Nguyen implement & | ^ ~

--- KTMT-1/QInt.h
+++ KTMT-1/QInt.h
@@ -22,15 +22,15 @@ public:
 	void operator << (int x);
 	void rol();
 	void ror();
-	QInt operator & (QInt& other);
-	QInt operator | (QInt& other);
-	QInt operator ^ (QInt& other);
-	QInt operator ~ ();
+	QInt& operator & (QInt& other);
+	QInt& operator | (QInt& other);
+	QInt& operator ^ (QInt& other);
+	QInt& operator ~ ();
 	QInt operator + (QInt& other);
 	QInt operator - (QInt& other);
 	QInt operator * (QInt& other);
 	QInt operator / (QInt& other);
-	QInt operator = (QInt& other);
+	QInt& operator = (QInt& other);
 
 };
 
--- KTMT-1/main.cpp
+++ KTMT-1/main.cpp
@@ -8,19 +8,46 @@ using namespace std;
 int main() {
 	// Test phần input và output
 	// Sửa maxlength trong constant thành 8 bit cho dễ test ## 2^7 = 128
-	QInt* a;
-	a = new QInt(2, "11111110");
-	cout << a->getBinaryType() << endl;
+	//QInt* a;
+	//a = new QInt(2, "11111110");
+	//cout << a->getBinaryType() << endl;
+	////cout << a->getDecimalType() << endl;
+	//cout << a->getHexaType() << endl;
+	//delete a;
+	//a = new QInt(10, "-120");
+	//cout << a->getBinaryType() << endl;
+	//cout << a->getHexaType() << endl;
+	///*cout << a->getDecimalType() << endl;*/
+	//delete a;
+	//a = new QInt(16, "7F");
+	//cout << a->getBinaryType() << endl;
 	//cout << a->getDecimalType() << endl;
-	cout << a->getHexaType() << endl;
-	delete a;
-	a = new QInt(10, "-120");
-	cout << a->getBinaryType() << endl;
-	cout << a->getHexaType() << endl;
-	/*cout << a->getDecimalType() << endl;*/
-	delete a;
-	a = new QInt(16, "7F");
-	cout << a->getBinaryType() << endl;
-	cout << a->getDecimalType() << endl;
-	delete a;
+	//delete a;
+
+
+	/*int x = 8;
+	int y = 9;
+
+	cout << (x | y) << endl;*/
+
+	QInt* a, * b;
+	a = new QInt(2, "10110001");
+	b = new QInt(2, "11001");
+	QInt* c;
+
+	c = &(*a & *b);
+	cout << "&: " << c->getBinaryType() << endl;
+	delete c;
+
+	c = &(*a | *b);
+	cout << "|: " << c->getBinaryType() << endl;
+	delete c;
+
+	c = &(*a ^ *b);
+	cout << "^: " << c->getBinaryType() << endl;
+	delete c;
+
+	c = &(~*a);
+	cout << "~: " << c->getBinaryType() << endl;
+	delete c;
 }
